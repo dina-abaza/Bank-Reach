18d4d47302947b4fe736a9b58658829643457dfa
RAMADAN-MAHDY <ramadanmahdy45@gmail.com>
2026-07-02 13:40:03 +0300

    feat: add WhatsAppEmbeddedSignup component and integrate with landing page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  redirect('/dashboard');
+  // redirect('/dashboard');
+
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <h1>Hello</h1>
+      <div id='home'>
+        {/* محتوى توجيهي عباره عن تيكست بتصميم مريح ومفهوم للزائر*/}
+        <p className='text-red-500 bg-blue-500 p-2 text-white text-xl font-bold shadow'>مرحبا بك في موقعنا</p>
+        <p className='text-red-500 bg-blue-500 p-2 text-white text-xl font-bold shadow'>هذا الموقع مخصص لتقديم الخدمات المصرفية</p>
+        <p className='text-red-500 bg-blue-500 p-2 text-white text-xl font-bold shadow'>يمكنك الاستفادة من خدماتنا المتنوعة</p>
+        <p className='text-red-500 bg-blue-500 p-2 text-white text-xl font-bold shadow'>للمزيد من المعلومات يرجى زيارة <a href="/dashboard">لوحة التحكم</a></p>  
+        <p className='text-red-500 bg-blue-500 p-2 text-white text-xl font-bold shadow'>لتجربة الخدمات المصرفية يرجى <a href="/connect">تسجيل الدخول</a></p>  
+      </div>
+    </div>
+  );
 }
